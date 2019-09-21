1706c5ca64f45b8279d01dfbc1f19ace6349123a
Vishal chaturvedi <vc121356@gmail.com>
2019-09-21 15:34:35 +0530

    ReactNative>=51 update

--- android/src/main/java/com/wix/autogrowtextinput/AutoGrowTextInputPackage.java
+++ android/src/main/java/com/wix/autogrowtextinput/AutoGrowTextInputPackage.java
@@ -18,11 +18,6 @@ public class AutoGrowTextInputPackage implements ReactPackage {
         return modules;
     }
 
-    // Deprecated in RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
